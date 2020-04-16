eae820041f3c0b01febb85ccccea1c4694aabe4c
meeseeks <meeseeks@iod.net>
2020-04-16 11:02:55 -0400

    Dynamicly augment sys.path with `src` directories.

--- src/sub_project/tests/conftest.py
+++ src/sub_project/tests/conftest.py
@@ -1,6 +1,21 @@
+import sys
+from pathlib import Path
+from typing import Generator
+
 import pytest
 
-from big_project.module0 import Module0
+# TODO: Add to test_support project's package __init__.py
+# PROJECT_ROOT: Path = (Path(__file__).parent / '..').resolve()
+PROJECT_ROOT: Path = Path.cwd()
+ALL_PROJECT_SRC_DIRS: Generator[Path, None, None] = PROJECT_ROOT.rglob('src')
+
+for tested_src in ALL_PROJECT_SRC_DIRS:
+    if tested_src.is_dir() and str(tested_src) not in sys.path:
+        print(f'Adding {tested_src} to sys.path')
+        sys.path.append(str(tested_src))
+
+from sub_project import Module0
+
 # noinspection PyUnresolvedReferences
 from sub_project_test_support.fixtures import sub0_module1_fixture
 
--- tests/big_project/conftest.py
+++ tests/big_project/conftest.py
@@ -1,4 +1,18 @@
+import sys
+from pathlib import Path
+from typing import Generator
+
 import pytest
+
+# TODO: Add to test_support project's package __init__.py
+# PROJECT_ROOT: Path = (Path(__file__).parent / '..').resolve()
+PROJECT_ROOT: Path = Path.cwd()
+ALL_PROJECT_SRC_DIRS: Generator[Path, None, None] = PROJECT_ROOT.rglob('src')
+
+for tested_src in ALL_PROJECT_SRC_DIRS:
+    if tested_src.is_dir() and str(tested_src) not in sys.path:
+        print(f'Adding {tested_src} to sys.path')
+        sys.path.append(str(tested_src))
 
 from sub_project.module0 import Module0
 # noinspection PyUnresolvedReferences
