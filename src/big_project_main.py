60725d47ecbbc5affbc477696fe39bfd52d8089f
meeseeks <meeseeks@iod.net>
2020-04-17 15:50:14 -0400

    Convert adding src to sys.path to a function.

--- src/big_project_main.py
+++ src/big_project_main.py
@@ -2,20 +2,27 @@ import sys
 from pathlib import Path
 from typing import Generator
 
-PROJECT_ROOT: Path = Path.cwd()
-ALL_PROJECT_SRC_DIRS: Generator[Path, None, None] = PROJECT_ROOT.rglob('src')
 
-for tested_src in ALL_PROJECT_SRC_DIRS:
-    if tested_src.is_dir() and str(tested_src) not in sys.path:
-        print(f'Adding {tested_src} to sys.path')
-        sys.path.append(str(tested_src))
-    else:
-        print(f'NOT adding {tested_src} to sys.path')
+# TODO: Add to test_support project's package __init__.py
+def add_src_to_sys_path():
+    all_project_src_dirs: Generator[Path, None, None] = Path.cwd().rglob('src')
+    prior_sys_path = sys.path.copy()
+    for tested_src in all_project_src_dirs:
+        if tested_src.is_dir() and str(tested_src) not in sys.path:
+            sys.path.append(str(tested_src))
+    print(f'Added to sys.path: {set(prior_sys_path).symmetric_difference(set(sys.path))}')
 
+
+add_src_to_sys_path()
+
+# pylint: disable=import-error,wrong-import-position
 import big_project
 from big_project import Module0
 
+# pylint: enable=import-error,wrong-import-position\
+
 print("Execute the Big Project.")
+
 
 def main():
     big_prog_mod0: Module0 = big_project.Module0()
--- src/sub_project/tests/conftest.py
+++ src/sub_project/tests/conftest.py
@@ -4,18 +4,21 @@ from typing import Generator
 
 
 # TODO: Add to test_support project's package __init__.py
-# PROJECT_ROOT: Path = (Path(__file__).parent / '..').resolve()
-PROJECT_ROOT: Path = Path.cwd()
-ALL_PROJECT_SRC_DIRS: Generator[Path, None, None] = PROJECT_ROOT.rglob('src')
+def add_src_to_sys_path():
+    all_project_src_dirs: Generator[Path, None, None] = Path.cwd().rglob('src')
+    prior_sys_path = sys.path.copy()
+    for tested_src in all_project_src_dirs:
+        if tested_src.is_dir() and str(tested_src) not in sys.path:
+            sys.path.append(str(tested_src))
+    print(f'Added to sys.path: {set(prior_sys_path).symmetric_difference(set(sys.path))}')
 
-for tested_src in ALL_PROJECT_SRC_DIRS:
-    if tested_src.is_dir() and str(tested_src) not in sys.path:
-        print(f'Adding {tested_src} to sys.path')
-        sys.path.append(str(tested_src))
+
+add_src_to_sys_path()
 
 # Import sub_project_test_support_fixture0 from the sub-project's fixtures made available to this test module via
 # the sub-project's 'src/sub_project_test_support' package. Ignore static checking that the import is not directly
 # referenced in this module.
+# pylint: disable=import-error,wrong-import-position
 # noinspection PyUnresolvedReferences
 from sub_project_test_support.fixtures import sub_project_test_support_fixture0
-
+# pylint: enable=import-error,wrong-import-position
