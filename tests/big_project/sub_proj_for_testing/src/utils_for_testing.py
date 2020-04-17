371a804f84523d848f0817fc0614fd0d9b0e89a5
meeseeks <meeseeks@iod.net>
2020-04-16 23:10:49 -0400

    Created 'sub_prog_for_testing' to be a sub-project that is only used in testing 'big_project'.

---
+++ tests/big_project/sub_proj_for_testing/src/utils_for_testing.py
@@ -0,0 +1,3 @@
+
+def util0() -> str:
+    return 'sub_project_test_support:test_utils.py:util0'
---
+++ tests/big_project/sub_proj_for_testing/tests/test_module0_for_testing.py
@@ -0,0 +1,5 @@
+from module0_for_testing import Module0ForTesting
+
+
+def test_func0():
+    assert Module0ForTesting().func0() == 'sub_pro_for_testing:module0_for_testing.py:Module0:func0'
